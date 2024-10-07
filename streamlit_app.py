86bf146474972e690f3da22e00ebba2846a306e1
sekharkafle <skafle@gmail.com>
2024-10-07 13:31:56 -0400

    updated code

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,75 +4,106 @@ import streamlit as st
 import requests
 import json
 from langchain_community.utilities import SQLDatabase
+from langchain_core.prompts import PromptTemplate
+import sqlite3
 
 # Show the page title and description.
-st.set_page_config(page_title="Movies dataset", page_icon="🎬")
-st.title("🎬 Movies dataset")
+st.set_page_config(page_title="Music dataset", page_icon="♪")
+st.title("♪ Music dataset")
 st.write(
     """
-    This app visualizes data from [The Movie Database (TMDB)](https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata).
-    It shows which movie genre performed best at the box office over the years. Just 
-    click on the widgets below to explore!
+    This app visualizes data from CHINOOK database (https://www.kaggle.com/datasets/nancyalaswad90/chinook-sample-database)
+    It includes 11 tables and Over 15,000 rows of data.
     """
 )
 
+if 'api_key' not in st.session_state:
+    st.session_state['api_key'] = None
 
-url = 'https://f4wo0au9r5.execute-api.us-east-1.amazonaws.com/default/claude-chat'
-myobj = {"messages":[{"role":"user","content":"hi"}]}
+if not st.session_state.api_key:
+    #with st.sidebar:
+    api_key=st.text_input("Enter password to get started:", type="password")
+    if api_key == "F@k3pa$$word":
+        st.session_state['api_key'] =api_key
+        st.rerun()
 
-x = requests.post(url, json = myobj)
-st.write(x.content.decode("utf-8").strip())
+if st.session_state['api_key']:
+    questions = ["Find all albums for the artist 'AC/DC'.",
+        "Find the total duration of all tracks.",
+        "List all customers from Canada.",
+        "Find the total number of invoices.",
+        "List top 5 tracks that are longer than 5 minutes.",
+        "Who are the top 5 customers by total purchase?"]
 
-# Load the data from a CSV. We're caching this so it doesn't reload every time the app
-# reruns (e.g. if the user interacts with the widgets).
-@st.cache_data
-def load_data():
-    #df = pd.read_csv("data/movies_genres_summary.csv")
-    #return df
+    url = 'https://f4wo0au9r5.execute-api.us-east-1.amazonaws.com/default/claude-chat'
 
-    db = SQLDatabase.from_uri("sqlite:///data//Chinook.db")
-    return db
+    def load_data():
+        db = SQLDatabase.from_uri("sqlite:///Chinook.db")
+        return db
+
+    def get_ai_response(obj):
+        x = requests.post(url, json = obj)
+        return x.content.decode("utf-8")
+
+    if not st.session_state.get("input_text"):
+        st.session_state.input_text = ""
+    system_message_sql = """
+        You are a {dialect} expert. You are interacting with a user who is asking you questions about the database.
+        Based on the table schema below, write a SQL query that would answer the user's question.
+        
+        Here is the database schema:
+        <schema>{table_info}</schema>
+        
+        Write ONLY THE SQL QUERY and nothing else. 
+        Do not wrap the SQL query in any other text, not even backticks. Do not include newline character in sql and return as a single line.
+        For example:
+        Question: Name 10 artists
+        SQL Query: <sql_query> SELECT Name FROM Artist LIMIT 10; <sql_query>
+        Your turn:
+        Question: {question}
+        SQL Query:
+        """
+
+    system_message_full = """
+        Based on the question from the user, sql_query, and sql_response, write a natural language response.
+        Do not exaplain the whole process of extracting response. Only provide the info on database result without any other information.
+        SQL Query: <sql_query> {query} </sql_query>
+        User question: {question}
+        SQL Response: {response}"""
+    with st.sidebar:
+        st.write("Sample Queries:")
+        for q in questions:    
+            if st.button(q):
+                st.session_state.input_text = q
+    text_input = st.text_input("Enter Query", st.session_state.input_text)
+    if text_input:
+        st.session_state.input_text = text_input
+
+    if len(st.session_state.input_text)>0:
+
+        question = st.session_state.input_text
+        db = load_data()
+        prompt = PromptTemplate.from_template(system_message_sql)
+        sys_message = prompt.format(dialect=db.dialect, table_info=db.get_table_info(),question=question)
+
+        human_message = {"role": "user", "content": question}
+        system_message = {"role": "system", "content": sys_message}
 
 
-df = load_data()
-
-# Show a multiselect widget with the genres using `st.multiselect`.
-genres = st.multiselect(
-    "Genres",
-    df.genre.unique(),
-    ["Action", "Adventure", "Biography", "Comedy", "Drama", "Horror"],
-)
-
-# Show a slider widget with the years using `st.slider`.
-years = st.slider("Years", 1986, 2006, (2000, 2016))
-
-# Filter the dataframe based on the widget input and reshape it.
-df_filtered = df[(df["genre"].isin(genres)) & (df["year"].between(years[0], years[1]))]
-df_reshaped = df_filtered.pivot_table(
-    index="year", columns="genre", values="gross", aggfunc="sum", fill_value=0
-)
-df_reshaped = df_reshaped.sort_values(by="year", ascending=False)
-
-
-# Display the data as a table using `st.dataframe`.
-st.dataframe(
-    df_reshaped,
-    use_container_width=True,
-    column_config={"year": st.column_config.TextColumn("Year")},
-)
-
-# Display the data as an Altair chart using `st.altair_chart`.
-df_chart = pd.melt(
-    df_reshaped.reset_index(), id_vars="year", var_name="genre", value_name="gross"
-)
-chart = (
-    alt.Chart(df_chart)
-    .mark_line()
-    .encode(
-        x=alt.X("year:N", title="Year"),
-        y=alt.Y("gross:Q", title="Gross earnings ($)"),
-        color="genre:N",
-    )
-    .properties(height=320)
-)
-st.altair_chart(chart, use_container_width=True)
+        sql_response = get_ai_response({"messages":[human_message, system_message]})
+        sql_response = sql_response[1:-1].strip()
+        sql_response = sql_response.replace("\\n", " ")
+        #st.write(sql_response)
+        db_res = db.run(sql_response)
+        prompt = PromptTemplate.from_template(system_message_full)
+        sys_message_txt = prompt.format(query=sql_response, response=db_res, question=question)
+        system_message_full = {"role": "system", "content": sys_message_txt}
+        #full_response = get_ai_response({"messages":[human_message, system_message_full]})
+        #st.write(db_res)
+        #st.write(full_response[1:-1].strip())
+        con = sqlite3.connect("Chinook.db")
+        df = pd.read_sql_query(sql_response, con)
+        st.dataframe(
+        df,
+        use_container_width=True)
+        con.close()
